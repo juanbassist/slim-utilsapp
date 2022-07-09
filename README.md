f83433144d17602d13d768bfb7c69164263d64da
Juan Ramirez <juanluisramirezm@gmail.com>
2022-07-09 13:02:44 -0500

    1.0.0

---
+++ README.md
@@ -0,0 +1,20 @@
+# Utils Apps 
+
+Provides tools to aid developer activities.
+
+# Tecnhologies
+
+- PHP 8
+- Slim 4 Framework
+
+# Dev Environent
+
+composer install
+
+# Changelog
+
+1.0.0
+- begin with readme and Changelog.
+- htaccess resolved for shared hosting provider.
+- add api endpoint to get the app version from composer.json.
+- http templates for vscode rest client with env conf file.
--- public/index.php
+++ public/index.php
@@ -8,10 +8,49 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
 
-$app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world5!");
-    return $response;
+$app->get('/api', function (Request $req, Response $res, $args) {    
+    $composerJsonFile = file_get_contents('../composer.json');
+    $composerJsonContent = json_decode($composerJsonFile, true);
+
+    $res->getBody()->write(json_encode([
+        'version' => $composerJsonContent['version'],
+    ]));
+
+    return $res
+              ->withHeader('Content-Type', 'application/json')
+              ->withStatus(200);
 });
+
+$app->get('/jsontoxml', function (Request $req, Response $res, $args) {
+    $jsonDataUrl = $req->getQueryParams()['jsonDataUrl'];
+
+    $jsonDataStr = file_get_contents($jsonDataUrl);
+    $obj = json_decode($jsonDataStr, true);    
+
+    $xml = new SimpleXMLElement('<root/>');
+    to_xml($xml, $obj);
+    
+    $res->getBody()->write($xml->asXML());
+    return $res
+              ->withHeader('Content-Type', 'application/xml')
+              ->withStatus(200);
+});
+
+$errorMiddleware = $app->addErrorMiddleware(true, true, true);
 
 $app->run();
 
+function to_xml(SimpleXMLElement $object, array $data)
+{   
+    foreach ($data as $key => $value) {
+        if (is_array($value)) {
+            $new_object = $object->addChild($key);
+            to_xml($new_object, $value);
+        } else {
+            if ($key != 0 && $key == (int) $key) {
+                $key = "key_$key";
+            }
+            $object->addChild($key, $value);
+        }   
+    }   
+}  
